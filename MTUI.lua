e80ce4119a7bf825da3cea97be7d72d5fdf22756
Martin <m@codedrecordings.com>
2022-01-16 16:39:28 +0100

    Compass tint, fix scopes/triggers

--- MTUI.lua
+++ MTUI.lua
@@ -1,45 +1,6 @@
 MTUI = {};
 
-function MTUI:ToggleMusic()
-    local s = GetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_MUSIC_ENABLED);
-    if (s == "1") then
-	SetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_MUSIC_ENABLED, "0");
-	d("Music disabled");
-    else
-        SetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_MUSIC_ENABLED, "1");
-        d("Music enabled");
-    end;
-end;
-
-function MTUI:SetMasterVolume(level)
-    level = tonumber(level);
-    SetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_AUDIO_VOLUME, level);
-    d("Master volume: "..level);
-end;
-
-function MTUI:DecreaseMasterVolume()
-    local v = tonumber(GetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_AUDIO_VOLUME));
-
-    if (v == 0) then return;
-    elseif (v > 75) then MTUI:SetMasterVolume(75);
-    elseif (v > 50) then MTUI:SetMasterVolume(50);
-    elseif (v > 25) then MTUI:SetMasterVolume(25);
-    else MTUI:SetMasterVolume(0);
-    end;
-end;
-
-function MTUI:IncreaseMasterVolume()
-    local v = tonumber(GetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_AUDIO_VOLUME));
-
-    if (v == 100) then return;
-    elseif (v < 25) then MTUI:SetMasterVolume(25);
-    elseif (v < 50) then MTUI:SetMasterVolume(50);
-    elseif (v < 75) then MTUI:SetMasterVolume(75);
-    else MTUI:SetMasterVolume(100);
-    end;
-end;
-
-function MTUI:SelectQuickslot(delta)
+local function SelectQuickslot(delta)
     local slots = { 12, 11, 10, 9, 16, 15, 14, 13 };
     local curr = GetCurrentQuickslot();
     local index;
@@ -55,16 +16,14 @@ function MTUI:SelectQuickslot(delta)
     PlaySound("Click_Positive");
 end;
 
-function MTUI:NextQuickslot()
-    MTUI:SelectQuickslot(1);
-end;
+local function TintCompass(r, g, b)
+    ZO_ReticleContainerReticle:SetColor(0, 1, 0);
+    ZO_CompassFrameLeft:SetColor(r, g, b, 1);
+    ZO_CompassFrameCenter:SetColor(r, g, b, 1);
+    ZO_CompassFrameRight:SetColor(r, g, b, 1);
+end
 
-function MTUI:PrevQuickslot()
-    MTUI:SelectQuickslot(-1);
-end;
-
--- Update attribute bars offsetX based on the shift set in settings.
-local function UpdateAttributeBarsOffsetX()
+local function TightenAttributeBares()
     local offsetX = 70;
     ZO_PlayerAttributeMagicka:ClearAnchors()
     ZO_PlayerAttributeMagicka:SetAnchor(RIGHT, ZO_PlayerAttributeHealth, LEFT, -offsetX, 0);
@@ -72,11 +31,61 @@ local function UpdateAttributeBarsOffsetX()
     ZO_PlayerAttributeStamina:SetAnchor(LEFT, ZO_PlayerAttributeHealth, RIGHT, offsetX, 0);
 end;
 
-EVENT_MANAGER:RegisterForEvent("MTUI_OnLoad", EVENT_ADD_ON_LOADED, function()
-    function KEYBINDING_MANAGER:IsChordingAlwaysEnabled()
-        return true;
-    end;
+-- Public methods
+function MTUI:NextQuickslot()
+    SelectQuickslot(1);
+end;
 
+function MTUI:PrevQuickslot()
+    SelectQuickslot(-1);
+end;
+
+function MTUI:ToggleMusic()
+    local s = GetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_MUSIC_ENABLED);
+    if (s == "1") then
+	SetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_MUSIC_ENABLED, "0");
+	d("Music disabled");
+    else
+        SetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_MUSIC_ENABLED, "1");
+        d("Music enabled");
+    end;
+end;
+
+local function SetMasterVolume(level)
+    level = tonumber(level);
+    SetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_AUDIO_VOLUME, level);
+    d("Master volume: "..level);
+end;
+
+function MTUI:DecreaseMasterVolume()
+    local v = tonumber(GetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_AUDIO_VOLUME));
+
+    if (v == 0) then return;
+    elseif (v > 75) then SetMasterVolume(75);
+    elseif (v > 50) then SetMasterVolume(50);
+    elseif (v > 25) then SetMasterVolume(25);
+    else SetMasterVolume(0);
+    end;
+end;
+
+function MTUI:IncreaseMasterVolume()
+    local v = tonumber(GetSetting(SETTING_TYPE_AUDIO, AUDIO_SETTING_AUDIO_VOLUME));
+
+    if (v == 100) then return;
+    elseif (v < 25) then SetMasterVolume(25);
+    elseif (v < 50) then SetMasterVolume(50);
+    elseif (v < 75) then SetMasterVolume(75);
+    else SetMasterVolume(100);
+    end;
+end;
+
+
+-- override global function
+function KEYBINDING_MANAGER:IsChordingAlwaysEnabled()
+    return true;
+end;
+
+EVENT_MANAGER:RegisterForEvent("MTUI_OnLoad", EVENT_ADD_ON_LOADED, function()
     SetCVar('SkipPregameVideos', '1');
     ZO_CreateStringId('SI_BINDING_NAME_MTUI_INCREASE_MASTER_VOLUME', 'Increase master volume');
     ZO_CreateStringId('SI_BINDING_NAME_MTUI_DECREASE_MASTER_VOLUME', 'Decrease master volume');
@@ -87,6 +96,14 @@ end);
 
 EVENT_MANAGER:RegisterForEvent('MTUI_OnPlayerActive', EVENT_PLAYER_ACTIVATED, function()
     ZO_ActionButtons_ToggleShowGlobalCooldown();
-    UpdateAttributeBarsOffsetX();
+    TightenAttributeBares();
+end);
+
+EVENT_MANAGER:RegisterForEvent('MTUI_CombatState', EVENT_PLAYER_COMBAT_STATE, function(code, inCombat)
+    if inCombat then
+        TintCompass(1, 0.75, 0.75);
+    else
+        TintCompass(1, 1, 1);
+    end
 end);
 
--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 * Decrease spacing between health/stam/mag bars
 * Enable key-chord bindings
 * Auto-skip startup logos
+* Tint compass red in combat
 * Some useful keybinds
    * Master volume +/- 25
    * Toggle music enable
